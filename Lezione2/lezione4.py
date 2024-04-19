1892555800a172f7575c2fd65a965940adc836a6
drahauts <danilarogowzow@gmail.com>
2024-04-19 12:11:11 +0200

    Exerc. N.1 lezione4

---
+++ Lezione2/lezione4.py
@@ -0,0 +1,12 @@
+# Exercise N.1
+
+def subtract(x: float, y: float):
+    print(f"Our x = {x} and our y = {y}")
+    return x - y
+
+x: float = float(input("Insert your first number: "))
+y: float = float(input("Insert your second number: "))
+
+print(subtract(x, y))
+
+# Exercise N.2
