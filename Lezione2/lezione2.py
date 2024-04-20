04e89ede4b4e39ddab07d83495e6e2d725a52834
drahauts <danilarogowzow@gmail.com>
2024-04-20 23:45:52 +0200

    Risolto fino ex. 6.3

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -182,4 +182,49 @@ def my_func_lesson2(my_list: list) -> list:
     
     return my_list
 
-print(my_func_lesson2(["New York", "Los Angeles", "Paris", "London", "Tokyo"]))  
+print(my_func_lesson2(["New York", "Los Angeles", "Paris", "London", "Tokyo"]))
+
+# 6-1. Person: Use a dictionary to store information about a person you know. Store their first name, last name, age, and the city in which they live.
+# You should have keys such as first_name, last_name, age, and city. Print each piece of information stored in your dictionary.
+
+danila_dict: dict = {
+    "first_name" : "Danila",
+    "last_name" : "Rahautsou",
+    "age" : 21,
+    "city" : "Rome"
+}
+
+for keys, value in danila_dict.items():
+    print(keys, value)
+
+# 6-2. Favorite Numbers: Use a dictionary to store people’s favorite numbers. Think of five names, and use them as keys in your dictionary.
+# Think of a favorite number for each person, and store each as a value in your dictionary. Print each person’s name and their favorite number.
+# For even more fun, poll a few friends and get some actual data for your program.
+
+favorite_number: dict = {
+    "Alice" : 5,
+    "Bob" : 21,
+    "Charlie" : 42,
+    "Emily" : 7,
+    "Jack" : 96
+}
+
+for name, num in favorite_number.items():
+    print(name, num)
+
+
+# 6-3. Glossary: A Python dictionary can be used to model an actual dictionary. However, to avoid confusion, let’s call it a glossary.
+# • Think of five programming words you’ve learned about in the previous chapters. Use these words as the keys in your glossary, and store their meanings as values.
+# • Print each word and its meaning as neatly formatted output. You might print the word followed by a colon and then its meaning, or print the word on one line and
+# then print its meaning indented on a second line. Use the newline character (\n) to insert a blank line between each word-meaning pair in your output.
+
+glossary: dict = {
+    "Problem" : "A task or question that requires a solution or answer.",
+    "Algorithm" : "An abstract step-by-step procedure to solve a problem.",
+    "Program" : "A set of instructions telling your computer how to execute an algorithm.",
+    "Data types" : "Different kinds of values that a program must represent to work with.",
+    "Variables" : "Named storage locations in a program's memory, used to hold data that can be modified during program execution."
+}
+
+for key, values in glossary.items():
+    print(f"In programming, the term {key} means {values}")
