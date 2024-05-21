a027e56fe969c4409ec00f0264f2d5618a002499
drahauts <danilarogowzow@gmail.com>
2024-05-21 18:31:09 +0200

    file binary_to_dec

---
+++ Lezione/binary_to_dec.py
@@ -0,0 +1,13 @@
+def binary_to_dec(x: str):
+    # 1011
+    x : list[str] = list(x)
+    # x -> ["1", "0", "1", "1"]
+    #        3    2    1    0
+    num: int = 0
+    for i in range(len(x)):
+        num += int(x[i]) * 2**(len(x)-i-1)
+    return num
+
+print(binary_to_dec("10001"))
+print(binary_to_dec("1100"))
+print(binary_to_dec("1010"))
