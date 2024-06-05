5732b8dfe6d087313821b04f3bb0a738e1125ceb
drahauts <danilarogowzow@gmail.com>
2024-06-05 13:52:39 +0200

    push

--- Lezione/prove.py
+++ Lezione/prove.py
@@ -1,22 +1,34 @@
-city1 = "Moscow"
-city2 = "Paris"
-city3 = "Lion"
+class Charachter:
 
-a, b, c = len(city1), len(city2), len(city3)
-abc = [a, b, c]
+    def __init__(self, level: int) -> None:
+        self.level = level
+        self.health = self.base_hp * level
+        self.attack_power = self.base_attack_power * level
 
+    def attack(self):
+        print(f"{self.base_name}: attacks with {self.attack_power} power")
 
-# if len(city1) < len(city2) or len(city1) < len(city3):
-#     print(city1)
-# elif len(city2) < len(city1) or len(city2) < len(city3):
-#     print(city2)
-# elif len(city3) < len(city2) or len(city3) < len(city1):
-#     print(city3)
+    def __str__(self) -> str:
+        return f"{self.base_name} (level - {self.level}, health - {self.health})"
+    
 
+class Ork(Charachter):
+    base_hp = 100
+    base_attack_power = 10
+    base_name = "Ork"
 
-# if len(city1) > len(city2) or len(city1) > len(city3):
-#     print(city1)
-# elif len(city2) > len(city1) or len(city2) > len(city3):
-#     print(city2)
-# else:
-#     print(city3)
+class Elf(Charachter):
+    base_hp = 50
+    base_attack_power = 15
+    base_name = "Elf"
+
+    def attack(self):
+        print("This method in the class-father")
+
+elf_1 = Elf(level= 1)
+print(elf_1)
+elf_1.attack()
+print("=======================")
+ork_1 = Ork(level= 1)
+print(ork_1)
+ork_1.attack()
